58af5989f9881d4fb7bd5cf2bd072c4c384e0dc7
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-01-06 16:43:39 -0800

    Added routes and views for /, /books, /books/new, and a POST for /books/new

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ sequelize.authenticate();
 sequelize.sync();
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -23,17 +22,16 @@ app.use(cookieParser());
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   const err = new Error(`Sorry, we can't find what you are looking for.`);
   err.status = 404;
   next(err)
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   if(!err.message && !err.status) {
     err.message = "Oops, something went wrong."
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,35 @@ var router = express.Router();
 const Book = require('../models').Book;
 
 /* GET home page. */
-router.get('/', (async (req, res, next) => {
+router.get('/', (req, res) => {
   // res.render('index', { title: 'Express' });
-    const books = await Book.findAll();
-    const err = new Error();
-    next(err);
-}));
+    res.redirect('/books');
+});
+
+router.get('/books', async (req, res) => {
+  const books = await Book.findAll();
+  res.render('index', {books});
+});
+
+router.get('/books/new', (req, res) => {
+  res.render('new-book');
+});
+
+router.post('/books/new', async (req, res) => {
+  let book;
+  console.log(req.body)
+  try {
+    book = await Book.create(req.body);
+    res.redirect(`/books/${book.id}`);
+  } catch(error) {
+    if(error.name === "SequelizeValidationError") { // checking the error
+      book = await book.build(req.body);
+      res.render("books/new", {book, errors: error.errors});
+    } else {
+      throw error; // error caught in the asyncHandler's catch block
+    }
+  }
+});
+
 
 module.exports = router;
